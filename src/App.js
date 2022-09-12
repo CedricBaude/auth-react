29dce89224c0de97fc2201155e74605906308a39
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-12 16:48:48 +0200

    context api + validation (1)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Home from "./pages/Home";
 import Navbar from './components/Navbar';
 import SignUpModal from './components/SignUpModal';
 
+
 function App() {
   return (
     <>
-      <Navbar />
       <SignUpModal />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
