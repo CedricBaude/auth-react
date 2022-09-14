0a78a633304bbebb880cc7459b49485455f79958
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-14 11:28:30 +0200

    add logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../context/userContext';
 import { Link } from "react-router-dom";
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase-config';
 
 const Navbar = () => {
 
     const { toggleModals } = useContext(UserContext);
+    const navigate = useNavigate();
+    const logOut = async () => {
+        try {
+            await signOut(auth)
+            navigate("/")
+        } catch {
+            alert("For some reason we can't deconnect, please check your internet connexion and retry.")
+        }
+    }
+
     return (
         <nav className="navbar navbar-light bg-light px-4">
             <Link to="/" className="navbar-brand">
@@ -21,7 +34,7 @@ const Navbar = () => {
                 Sign In
             </button>
             <button
-
+                onClick={logOut}
                 className="btn btn-danger ms-2">
                 Logout
             </button>
